2aef96e64113d7bc78322a8732f63a41e3de8cc4
Danil L <danillitvinenkod00@gmail.com>
2024-10-02 21:02:50 +0300

    Created MenuList component & realized request to server

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react";
 import Headling from "../../components/Headling/Headling";
-import ProductCard from "../../components/ProductCard/ProductCard";
 import Search from "../../components/Search/Search";
+import { PREFIX } from "../../helpers/API";
+import { Product } from "../../interfaces/product.interface";
 import styles from "./Menu.module.css";
+import axios, { AxiosError } from "axios";
+import { MenuList } from "./MenuList/MenuList";
 
 export default function Menu() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | undefined>();
+
+  const getMenu = async () => {
+    try {
+      setIsLoading(true);
+      await new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 2000);
+      });
+      const { data } = await axios.get<Product[]>(`${PREFIX}/products`);
+      setProducts(data);
+      setIsLoading(false);
+    } catch (e) {
+      console.error(e);
+      if (e instanceof AxiosError) {
+        setError(e.message);
+      }
+      setIsLoading(false);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (
     <>
       <div className={styles["head"]}>
@@ -11,14 +44,9 @@ export default function Menu() {
         <Search placeholder="Введите блюдо или состав" />
       </div>
       <div>
-        <ProductCard
-          id={1}
-          title="Наслаждение"
-          description="Салями, руккола, помидоры, оливки"
-          rating={4.5}
-          price={300}
-          image="/product-demo.png"
-        />
+        {error && <>{error}</>}
+        {!isLoading && <MenuList products={products} />}
+        {isLoading && <>Загрузка продуктов...</>}
       </div>
     </>
   );
---
+++ src/pages/Menu/MenuList/MenuList.tsx
@@ -0,0 +1,20 @@
+import ProductCard from "../../../components/ProductCard/ProductCard";
+import { MenuListProps } from "./MenuList.props";
+
+export function MenuList({ products }: MenuListProps) {
+  return (
+    <>
+      {products.map((p) => (
+        <ProductCard
+          key={p.id}
+          id={1}
+          name={p.name}
+          description={p.ingredients.join(", ")}
+          rating={p.rating}
+          price={p.price}
+          image={p.image}
+        />
+      ))}
+    </>
+  );
+}
